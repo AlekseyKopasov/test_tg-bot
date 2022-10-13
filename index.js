464e7181f23d9b1650a00e94be84e35db10b4bd9
Aleksey Kopasov <alesey.k1@gmail.com>
2022-10-14 00:45:12 +0300

    добавляет игру в бот

--- index.js
+++ index.js
@@ -1,28 +1,56 @@
 const TelegramAPI = require('node-telegram-bot-api')
-const { BotToken } = require('./consts')
+const { gameOptions, againOptions } = require('./options')
 
-const token = BotToken
+const token = '5794343045:AAFS3144mR6xrwHbTLIRwRHXDfSTg6vMcFM'
 
 const bot = new TelegramAPI(token, { polling: true })
+
+const chats = {}
+
+const startGame = async (chatId) => {
+    await bot.sendMessage(chatId, `Сейчас я загадаю цифру от 0 до 9, а ты должен ее угадать!`)
+    const randomNumber = Math.floor(Math.random() * 10)
+    chats[chatId] = randomNumber
+    await bot.sendMessage(chatId, 'Отгадай!', gameOptions)
+}
 
 const start = () => {
     bot.setMyCommands([
         { command: '/start', description: 'Начальное приветствие' },
         { command: '/info', description: 'Получить информацию  о пользователе' },
+        { command: '/game', description: 'Игра угадай цифру' },
     ])
 
     bot.on('message', async msg => {
         const text = msg.text
-        const chatID = msg.chat.id
+        const chatId = msg.chat.id
 
         if (text === '/start') {
-            await bot.sendSticker(chatID, 'https://tlgrm.eu/_/stickers/ea5/382/ea53826d-c192-376a-b766-e5abc535f1c9/192/7.webp')
-            return bot.sendMessage(chatID, `Добро пожаловать!`)
+            await bot.sendSticker(chatId, 'https://tlgrm.eu/_/stickers/ea5/382/ea53826d-c192-376a-b766-e5abc535f1c9/192/7.webp')
+            return bot.sendMessage(chatId, `Добро пожаловать!`)
         }
         if (text === '/info') {
-            return bot.sendMessage(chatID, `Тебя зовут ${ msg.from.first_name } ${ msg.from.last_name }.`)
+            return bot.sendMessage(chatId, `Тебя зовут ${ msg.from.first_name } ${ msg.from.last_name }.`)
         }
-        return bot.sendMessage(chatID, 'Я тебя не понимаю!')
+        if (text === '/game') {
+            return startGame(chatId)
+        }
+        return bot.sendMessage(chatId, 'Я тебя не понимаю!')
+    })
+
+    bot.on('callback_query', async msg => {
+        const data = msg.data
+        const chatId = msg.message.chat.id
+
+        if (data === '/again') {
+            return startGame(chatId)
+        }
+
+        if (+data === +chats[chatId]) {
+            return bot.sendMessage(chatId, `Поздравляю, ты отгадал цифру ${ chats[chatId] }.`, againOptions)
+        } else {
+            return bot.sendMessage(chatId, `К сожалению, ты не отгадал, бот загадал цифру ${ chats[chatId] }.`, againOptions)
+        }
     })
 }
 
